9c6bf7a8c6588a89575209250310303714708602
unknown <104711209@student.swin.edu.au>
2023-09-16 16:17:44 +1000

    completed menu bar and titles

--- src/App.js
+++ src/App.js
@@ -4,22 +4,34 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      {/* Navbar */}
+           <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+         {/* This is the site logo */}
+          <div class="navbar-brand">
+          <a class="navbar-item" href="./">
+          <img src="logo192.png" alt="logo" width="28" height="28"/>
+          </a>
+
+          </div>     
+            {/*Nav bar links  */}
+          <div class="navbar-menu">
+            <div class="navbar-start">
+              <a class="navbar-item" href="./">Home</a>
+              <a class="navbar-item" href="./about">About</a>
+            </div>         
+          </div>
+        </nav>
+      {/* Page Contents */}      
+      <div class="container is-widescreen">
+      <section class="section">
+
+      <h1 class="title">Fitzy's Bodybuilding Consultancy Services</h1>
+      <h2 class="subtitle">Perfecting mind and muscle together</h2>
+      </section>
+      </div>
+   </div>
   );
 }
 
 export default App;
+
